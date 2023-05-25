131775ab3f835b32020d6f3719930298954dc471
Elena Venskaya <venskaya.04@mail.ru>
2023-05-25 18:49:26 +0500

    release v1

--- renta/main/urls.py
+++ renta/main/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
         path('basket', views.basket, name='basket'),
         path('catalog', views.catalog, name='catalog'),
         path('take', views.take, name='take'),
-        path('category', views.category, name='category')
+        path('getby', views.getby, name='getby'),
+        path('category', views.category, name='category'),
+        path('sport', views.sport, name='sport'),
+        path('child', views.child, name='child'),
+        path('cloth', views.cloth, name='cloth'),
+        path('other', views.other, name='other'),
+        path('rest', views.rest, name='rest'),
+
 ]
--- renta/main/views.py
+++ renta/main/views.py
@@ -14,8 +14,32 @@ def take(request):
     return render(request, 'main/take.html')
 
 
+def getby(request):
+    return render(request, 'main/getby.html')
+
+
 def category(request):
     return render(request, 'main/category.html')
+
+def sport(request):
+    return render(request, 'main/sport.html')
+
+
+def child(request):
+    return render(request, 'main/child.html')
+
+
+def cloth(request):
+    return render(request, 'main/cloth.html')
+
+
+def other(request):
+    return render(request, 'main/other.html')
+
+
+def rest(request):
+    return render(request, 'main/rest.html')
+
 
 
 def catalog(request):
